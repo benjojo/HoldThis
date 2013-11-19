c8a60ff8db6d6d19e0965177d9a321038ad98483
Ben Cox <ben@benjojo.co.uk>
2013-11-19 20:22:12 +0000

    Neat

---
+++ hold.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/codegangsta/cli"
+	"os"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "HoldThis - Holderr"
+	app.Usage = "$ Hold <name of hold>"
+	app.Action = func(c *cli.Context) {
+		if len(c.Args()) == 1 {
+			str, _ := os.Getwd()
+			println("Saving the directory", str, "as the name '", c.Args()[0], "'")
+		} else {
+			println(app.Usage)
+		}
+	}
+
+	app.Run(os.Args)
+}
