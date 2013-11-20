5fa0f05d2630fe224cbbe2af2039381a57db087f
Ben Cox <ben@benjojo.co.uk>
2013-11-20 09:15:30 +0000

    Eh. mornings

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ go build hold.go
 go build jump.go
 mv hold /bin/
 mv jump /bin/_jump
-echo 'function jump2() { $( _jump "$@" ) ;}' >> ~/.profile
-echo 'function jump2() { $( _jump "$@" ) ;}' >> ~/.bashrc
-echo 'function jump2() { $( _jump "$@" ) ;}' >> ~/.zshrc
+echo 'function jump() { $( _jump "$@" ) ;}' >> ~/.profile
+echo 'function jump() { $( _jump "$@" ) ;}' >> ~/.bashrc
+echo 'function jump() { $( _jump "$@" ) ;}' >> ~/.zshrc
 echo "Installed. Restart all shells to take affect"
