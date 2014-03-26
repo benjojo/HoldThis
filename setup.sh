99687bac420aa9ee7a3db1992e8983f4924a5f74
Ben Cox <ben@benjojo.co.uk>
2014-03-26 23:01:41 +0000

    Improved the setup script that installs the system
    Now it will stop running if any of th ecommands fail

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-go build hold.go
-go build jump.go
-mv hold /bin/
-mv jump /bin/_jump
-echo 'function jump() { $( _jump "$@" ) ;}' >> ~/.profile
-echo 'function jump() { $( _jump "$@" ) ;}' >> ~/.bashrc
-echo 'function jump() { $( _jump "$@" ) ;}' >> ~/.zshrc
+go build hold.go &&
+go build jump.go &&
+mv hold /bin/ &&
+mv jump /bin/_jump &&
+echo 'function jump() { $( _jump "$@" ) ;}' >> ~/.profile &&
+echo 'function jump() { $( _jump "$@" ) ;}' >> ~/.bashrc &&
+echo 'function jump() { $( _jump "$@" ) ;}' >> ~/.zshrc &&
 echo "Installed. Restart all shells to take affect"
