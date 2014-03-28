933abe331b436b3e24d2a01a06f22af427633cd8
Ben Cox <ben@benjojo.co.uk>
2014-03-28 13:34:31 +0000

    Added the ability to pass a pram to jumplist to refine a search

--- jumplist.go
+++ jumplist.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
+	fmt.Println(len(os.Args))
 	List := GetEntries()
 	for _, v := range List {
-		fmt.Printf("%s -> %s\n", v.Key, v.Value)
+		if len(os.Args) > 1 {
+			if strings.HasPrefix(v.Key, os.Args[1]) {
+				fmt.Printf("%s -> %s\n", v.Key, v.Value)
+			}
+		} else {
+			fmt.Printf("%s -> %s\n", v.Key, v.Value)
+		}
 	}
 }
